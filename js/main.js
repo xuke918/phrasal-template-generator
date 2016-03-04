a9142b0aee4246223a905fe62e0a8df5b81ae198
xuke918 <xuke3070@gmail.com>
2016-03-04 11:58:50 -0500

    Added var.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var colour = prompt('A colour (orange, indigo, etc.)')
+
+var creature = prompt('A creature (dragon, moth, etc.)')
+
+var adjective = prompt('A abjective (beautiful, super, etc.)')
+
+var pastTenseVerb = prompt('A past tense verb (ran, burped, etc.)')
