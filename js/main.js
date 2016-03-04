f424b29ef1b7494445b18a79a2db42a03b3113fd
xuke918 <xuke3070@gmail.com>
2016-03-04 12:51:28 -0500

    Added story text.

--- js/main.js
+++ js/main.js
@@ -6,4 +6,19 @@ var adjective = prompt('A abjective (beautiful, super, etc.)');
 
 var pastTenseVerb = prompt('A past tense verb (ran, burped, etc.)');
 
-var letter = prompt('Which story would you like?');
+var letter = prompt('Which story would you like?\n[a] Snaking\n[b] Escaping\n[c] Winning \nEnter the letter blow:');
+
+var abcStory = ['a','b','c'];
+
+switch (story) {
+
+case 'a' :
+After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.
+
+case 'b' :
+Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.
+
+case 'c' :
+Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.
+
+}
