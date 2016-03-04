766dde61127d7e77d798b6074a5c59c76ad1170b
xuke918 <xuke3070@gmail.com>
2016-03-04 14:05:52 -0500

    Fixed the spelling error.

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ var colour = prompt('A colour (orange, indigo, etc.)');
 
 var creature = prompt('A creature (dragon, moth, etc.)');
 
-var adjective = prompt('A abjective (beautiful, super, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
 
 var pastTenseVerb = prompt('A past tense verb (ran, burped, etc.)');
 
