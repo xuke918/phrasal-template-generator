d5b8531a1208b961902e1ef44ec3952e30246c44
xuke918 <xuke3070@gmail.com>
2016-03-04 14:25:01 -0500

    Fixed Markbot.

--- js/main.js
+++ js/main.js
@@ -6,22 +6,23 @@ var creature = prompt('A creature (dragon, moth, etc.)');
 
 var adjective = prompt('An adjective (beautiful, super, etc.)');
 
-var pastTenseVerb = prompt('A past tense verb (ran, burped, etc.)');
+var verbed = prompt('A past tense verb (ran, burped, etc.)');
 
-var letter = prompt('Which story would you like?\n[a] Snaking\n[b] Escaping\n[c] Winning \nEnter the letter blow:');
+var story = prompt('Which story would you like?\n[a] Snacking\n[b] Escaping\n[c] Winning\nEnter the letter below.');
 
 var abcStory = ['a', 'b', 'c'];
 
 switch (story) {
-  case 'a':
+
+  case 'a' :
     document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
     break;
 
-  case 'b':
-    document.write('Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.');
+  case 'b' :
+    document.write('Ameilia ' + verbed + ' through the ' + adjective + ' ' + colour + ' nebula escaping the space ' + creature + ' .');
     break;
 
-  case 'b':
-    document.write('Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.');
+  case 'c' :
+    document.write('Jackson chose his ' + adjective + ', ' + colour + ' ' + creature + ' card and ' + verbed + ' it to the table knowing he won.');
     break;
 }
