14501ba3fcc1893d01bd5411756b1fdc3b8df2a4
xuke918 <xuke3070@gmail.com>
2016-03-04 13:13:16 -0500

    Deleted unused elements.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var intro = alert('Let’s make a story together!')
+
 var colour = prompt('A colour (orange, indigo, etc.)');
 
 var creature = prompt('A creature (dragon, moth, etc.)');
@@ -8,19 +10,11 @@ var pastTenseVerb = prompt('A past tense verb (ran, burped, etc.)');
 
 var letter = prompt('Which story would you like?\n[a] Snaking\n[b] Escaping\n[c] Winning \nEnter the letter blow:');
 
-var abcStory = ['a','b','c'];
-
+var abcStory = ['a', 'b', 'c',]
 switch (story) {
+  case 'a':
+  document.write('After snacking on ' + adjective + ' treats, the ' + colour + ' bellied ' + creature + ' ' + verbed + ' for hours.');
+    break;
 
-case 'a' :
-  document.write(After snacking on {adjective} treats, the {colour} bellied {creature} {verbed} for hours.);
-  break;
 
-case 'b' :
-  document.write(Ameilia {verbed} through the {adjective} {colour} nebula escaping the space {creature}.);
-  break;
-
-case 'c' :
-  document.write(Jackson chose his {adjective}, {colour} {creature} card and {verbed} it to the table knowing he won.);
-  break;
 }
